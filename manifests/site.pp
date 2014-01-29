24418b3badd6535c9ac1541004f8d010caaed5ee
GervaisdeM <gervais@demontbrun.com>
2014-01-29 17:51:12 -0400

    removed sublime

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 node default {
   include puppet
   include skype
-  include sublime_text_2
-    sublime_text_2::package { 'Emmet':
-    source => 'sergeche/emmet-sublime'
-    }
+#  include sublime_text_2
+#    sublime_text_2::package { 'Emmet':
+#    source => 'sergeche/emmet-sublime'
+#    }
   include virtualbox
   include xquartz
 }
